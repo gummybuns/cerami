4714df0b933c7599ddd7281b822ba0bb2b4047d5
Zac Brown <zmb1391@gmail.com>
2020-03-12 23:19:57 -0400

    First crack at a setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="dorm",
+      version="0.1.0",
+      description="Dynamodb ORM for python",
+      author="Zac Brown",
+      author_email="gummybuns@protonmail.com",
+      package_dir={"dorm": "lib"})
