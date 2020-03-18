db1d48d5c22d9f82cfb10bf54436fac4dc2fd4e7
Zac Brown <zmb1391@gmail.com>
2020-03-18 02:11:49 -0400

    add new version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cerami",
-    version="0.1.0",
+    version="0.1.2",
     author="Zac Brown",
     author_email="gummybuns@protonmail.com",
     description="A Dynamodb ORM",
