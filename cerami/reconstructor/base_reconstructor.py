7f936d996020e235449e5196c0dc0416c33422da
Zac Brown <zmb1391@gmail.com>
2020-05-09 15:00:26 -0400

    Add comments for reconstructors

--- cerami/reconstructor/base_reconstructor.py
+++ cerami/reconstructor/base_reconstructor.py
@@ -1,2 +1,21 @@
 class BaseReconstructor(object):
-    pass
+    """The base class for all Reconstructors
+
+    Reconstructors are used to interpret data from a DynamoDB response. All
+    classes that inherit from BaseReconstructor implement reconstruct() which
+    takes in the DynamoDB data and manipulates it accordingly.
+
+    For example, the ModelReconstructor converts the raw DynamoDB response data back
+    into the Model of the corresponding table
+    """
+
+    def reconstruct(self, item_dict):
+        """The abstract reconstruct method to be implemented by child classes
+
+        Its purpose it to convert the item_dict from DynamoDB
+
+        Arguments:
+        item_dict -- a dict from DynamoDB. It will be in the format like
+        {"a_column": {"S": "my_value"}}
+        """
+        raise "Not implemented"
--- cerami/reconstructor/model_reconstructor.py
+++ cerami/reconstructor/model_reconstructor.py
@@ -1,10 +1,28 @@
 from .base_reconstructor import BaseReconstructor
 
 class ModelReconstructor(BaseReconstructor):
+    """A Reconstructor class to convert the DynamoDB response data back into a Model"""
     def __init__(self, model_cls):
+        """constructor for ModelReconstructor
+
+        Arguments:
+        model_cls -- the class of the Model to convert the data into
+        """
         self.model_cls = model_cls
 
     def reconstruct(self, item_dict):
+        """Convert the item_dict into a Model instance
+
+        Iterates over all columns in the model_cls and tries to set the value based on the
+        item_dict's keys
+
+        Arguments:
+        item_dict -- a dict from DynamoDB. It will be in the format like
+        {"a_column": {"S": "my_value"}}
+
+        Returns:
+        an instance of model_cls
+        """
         data = {}
         for column in self.model_cls._columns:
             try:
